a953c34280697b58f12a2fb2991fa7f04636ddf3
Klemens <klemenskraft@gmail.com>
2022-04-02 19:55:10 +0200

    Create button, which removes all squares and generate a new grid with a number the user put in a prompt

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
+
+
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < 255; i++) {
   let div = document.createElement('div');
   div.classList.add('square');
   container.appendChild(div);
 }
 
-const squares = document.querySelectorAll('.square');
+let squares = document.querySelectorAll('.square');
 for (let square of squares) {
   square.addEventListener('click', () => {
     square.style.backgroundColor = 'black';
   })
 }
+
+const btnClear = document.querySelector('#btn-clear');
+btnClear.addEventListener('click', () => {
+  for (let square of squares) {
+    container.removeChild(square);
+  }
+
+  let grid = prompt('Enter a number for the squares per side');
+  let width = 960 / grid;
+
+  for (let i = 0; i < grid * grid - 1; i++) {
+    let div = document.createElement('div')
+    div.classList.add('square');
+    div.style.width = `${width}px`;
+    container.appendChild(div);
+  }
+  squares = document.querySelectorAll('.square');
+  for (let square of squares) {
+    square.addEventListener('click', () => {
+      square.style.backgroundColor = 'black';
+    })
+  }
+})
